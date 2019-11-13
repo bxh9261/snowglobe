2f32a1bde79d4b4df8dc4104d57da67119332b04
Eddie Brazier <54645487+EddieBrazier@users.noreply.github.com>
2019-11-13 14:42:57 -0500

    working on making the screen fade to white in the scene

---
+++ 3D_Animation_Final/Assets/Scripts/FadeScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Eddie Brazier
+3D Animation Final
+*/
+
+public class FadeScreen : MonoBehaviour
+{
+    //make this class static
+    public static FadeScreen instance;
+
+    private void Awake()
+    {
+        //initialize static instance
+        if(instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void Fade()
+    {
+        StartCoroutine(StartFade());
+        //StartCoroutine(ReverseFade());
+    }
+
+    IEnumerator StartFade()
+    {
+        CanvasGroup canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        while(canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha += Time.deltaTime / 2;
+            yield return null;
+        }
+
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    IEnumerator ReverseFade()
+    {
+        CanvasGroup canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+}
---
+++ 3D_Animation_Final/Assets/Scripts/ScreenTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //method is activated when a collider exits the trigger
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Fade");
+
+        //fade the screen to white
+        FadeScreen.instance.Fade();
+
+        //teleport player back to start position
+        other.gameObject.transform.position = new Vector3(75, 1, 75);
+    }
+}

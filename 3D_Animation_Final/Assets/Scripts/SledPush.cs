2599c9924b94464091eb656f0f581716e5856d8e
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2019-12-09 18:42:48 -0500

    sled now slides and work has begun on candy cane guide

--- 3D_Animation_Final/Assets/Scripts/SledPush.cs
+++ 3D_Animation_Final/Assets/Scripts/SledPush.cs
@@ -9,24 +9,34 @@ using UnityEngine;
 public class SledPush : MonoBehaviour
 {
     //field for sled pushing power
-    public float sledForce = 0;
+    public float sledForce = 9;
 
     private bool sledBool = false;
+
+    //field for player
+    private GameObject player;
+
+    //timing fields
+    private float sledTimer = 7f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //get reference to player 
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Vector3.Distance(player.transform.position, gameObject.transform.position) <= 5f)
+        {
+            sledBool = true;
+        }
+
         if(sledBool == true)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * sledForce);
-
-            Debug.Log("pushing sled");
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.right * sledForce);
 
             sledForce += 1f * Time.deltaTime;
 
@@ -34,14 +44,19 @@ public class SledPush : MonoBehaviour
             {
                 sledForce = 20f;
             }
-        }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject == GameObject.FindGameObjectWithTag("Player"))
+            //increment sled timer
+            sledTimer -= 1 * Time.deltaTime;
+            if(sledTimer <= 0)
+            {
+                Destroy(this);
+            }
+        }
+
+        //debug command for sled pushing
+        if(Input.GetKeyDown(KeyCode.E))
         {
-            sledBool = true;
+            sledBool = !sledBool;
         }
     }
 
